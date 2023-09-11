f29d491efb5b2d2b7e2379916986f5de595c778e
vudala <vudalae@gmail.com>
2023-09-11 12:14:56 -0400

    adding more weather settings

--- weather.py
+++ weather.py
@@ -3,13 +3,22 @@ import airsim
 
 WIND_NORTH_SPEED_MS = 0
 WIND_EAST_SPEED_MS = 0
-WIND_DOWN_SPEED_MS = 8
+WIND_DOWN_SPEED_MS = 0
 
 # 0.0 - 1.0
-RAIN_INTENSITY = 0
+RAIN_INTENSITY = 0.0
 
 # 0.0 - 1.0
-DUST_CONCENTRATION = 0
+DUST_CONCENTRATION = 0.0
+
+# 0.0 - 1.0
+FOG_CONCENTRATION = 0.0
+
+# 0.0 - 1.0
+SNOW_CONCENTRATION = 0.0
+
+# 0.0 - 1.0
+MAPLE_LEAF_CONCENTRATION = 0.5
 
 
 client = airsim.MultirotorClient()
@@ -35,10 +44,30 @@ client.simSetWeatherParameter(
     DUST_CONCENTRATION
 )
 
+# fog concentration
+client.simSetWeatherParameter(
+    airsim.WeatherParameter.Fog,
+    FOG_CONCENTRATION
+)
+
+# snow concentration
+client.simSetWeatherParameter(
+    airsim.WeatherParameter.Snow,
+    SNOW_CONCENTRATION
+)
+
+# maple leaf concentration
+client.simSetWeatherParameter(
+    airsim.WeatherParameter.MapleLeaf,
+    MAPLE_LEAF_CONCENTRATION
+)
+
 input('Barrier')
 
 client.simSetWeatherParameter(airsim.WeatherParameter.Rain, 0)
 client.simSetWeatherParameter(airsim.WeatherParameter.Snow, 0)
 client.simSetWeatherParameter(airsim.WeatherParameter.Dust, 0)
+client.simSetWeatherParameter(airsim.WeatherParameter.Fog, 0)
+client.simSetWeatherParameter(airsim.WeatherParameter.MapleLeaf, 0)
 
 client.simEnableWeather(False)
